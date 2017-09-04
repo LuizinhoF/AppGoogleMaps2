49b7c7fc34a0399bcbd61df2df94f3e49c99174c
Diego Cardoso <dcardoso86@gmail.com>
2017-09-04 08:01:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/opet/tds/appgooglemaps2/MapsActivity.java
@@ -0,0 +1,153 @@
+package br.com.opet.tds.appgooglemaps2;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied;
+    /**
+     * A {@link LocationSource} which reports a new location whenever a user long presses the map
+     * at
+     * the point at which a user long pressed the map.
+     */
+    private static class LongPressLocationSource implements LocationSource, OnMapLongClickListener {
+
+        private OnLocationChangedListener mListener;
+
+        /**
+         * Flag to keep track of the activity's lifecycle. This is not strictly necessary in this
+         * case because onMapLongPress events don't occur while the activity containing the map is
+         * paused but is included to demonstrate best practices (e.g., if a background service were
+         * to be used).
+         */
+        private boolean mPaused;
+
+        @Override
+        public void activate(OnLocationChangedListener listener) {
+            mListener = listener;
+        }
+
+        @Override
+        public void deactivate() {
+            mListener = null;
+        }
+
+        @Override
+        public void onMapLongClick(LatLng point) {
+            if (mListener != null && !mPaused) {
+                Location location = new Location("LongPressLocationProvider");
+                location.setLatitude(point.latitude);
+                location.setLongitude(point.longitude);
+                location.setAccuracy(100);
+                mListener.onLocationChanged(location);
+            }
+        }
+
+        private void onPause() {
+            mPaused = true;
+        }
+
+        private void onResume() {
+            mPaused = false;
+        }
+    }
+
+    private LongPressLocationSource mLocationSource;
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        mLocationSource = new LongPressLocationSource();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationSource.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationSource.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setLocationSource(mLocationSource);
+        mMap.setOnMapLongClickListener(mLocationSource);
+        enableMyLocation();
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+}
---
+++ app/src/main/java/br/com/opet/tds/appgooglemaps2/Test.java
@@ -0,0 +1,15 @@
+package br.com.opet.tds.appgooglemaps2;
+
+import java.io.Serializable;
+
+/**
+ * Created by Diego on 30/08/2017.
+ */
+
+public class Test implements Serializable {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
